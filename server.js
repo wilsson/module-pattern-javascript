ab5d93c3b2c22dc7b74165da90ede8224f2b5e54
wilsson <wilsonft83@gmail.com>
2015-10-27 17:40:29 -0500

    chore(ejs):quitando jade por ejs

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 var express = require('express');
 var app = express();
 
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.set('views', __dirname+'/demo');
 app.use(express.static(__dirname+'/demo'));
 
